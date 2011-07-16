40a6901d7938f5f5b8a3cb4e601c997fd9ad1af3
Link Gou <gouboft@hotmail.com>
2011-07-17 00:55:44 +0800

    add cscope in .emacs

--- my-lisps/ecb-settings.el
+++ my-lisps/ecb-settings.el
@@ -3,10 +3,16 @@
  '(ecb-options-version "2.40")
  '(ecb-tip-of-the-day nil)
  '(ecb-windows-width 38)
- '(ecb-layout-name "right3")
+ '(ecb-layout-name "right4")
  '(ecb-methods-menu-sorter (lambda (entries) (let ((sorted (copy-list entries))) (sort sorted (quote string-lessp))))))
 (custom-set-faces
 )
 
+;; Key settings
+(global-set-key (kbd "C-l") 'ecb-goto-window-methods)
+(global-set-key (kbd "C-x ;") 'ecb-goto-window-sources)
+(global-set-key (kbd "C-;") 'ecb-goto-window-edit1)
+(global-set-key (kbd "C-x {") 'ecb-activate)
+(global-set-key (kbd "C-x }") 'ecb-deactivate)
 
 (provide 'ecb-settings)
