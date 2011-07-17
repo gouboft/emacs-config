5c22f545456c65cb7f6bd181bf68dd0a3d67fc75
Link Gou <gouboft@hotmail.com>
2011-07-17 17:43:37 +0800

    map key to semantic code jump

--- my-lisps/program-settings.el
+++ my-lisps/program-settings.el
@@ -37,4 +37,23 @@
 ;; Goto line
 (global-set-key [f5] 'goto-line)
 
+;; Semantic jump setting
+;; jump to define
+(global-set-key [f9] 'semantic-ia-fast-jump)
+;; jump back after jump to define
+(global-set-key [f10]
+                (lambda ()
+                  (interactive)
+                  (if (ring-empty-p (oref semantic-mru-bookmark-ring ring))
+                      (error "Semantic Bookmark ring is currently empty"))
+                  (let* ((ring (oref semantic-mru-bookmark-ring ring))
+                         (alist (semantic-mrub-ring-to-assoc-list ring))
+                         (first (cdr (car alist))))
+                    (if (semantic-equivalent-tag-p (oref first tag)
+                                                   (semantic-current-tag))
+                        (setq first (cdr (car (cdr alist)))))
+                    (semantic-mrub-switch-tags first))))
+
+
+
 (provide 'program-settings)
