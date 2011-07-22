e8d4fc185458f9ac5aa6023f09d45a4044727c42
Bo Gou <bgou@hzwowpad.com>
2011-07-22 20:37:28 +0800

    add evernote for emacs

--- my-lisps/misc-settings.el
+++ my-lisps/misc-settings.el
@@ -58,5 +58,15 @@
                          (interactive)
                          (popup-menu 'yank-menu)))
 
+;; Evernote mode
+(setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8")) ; option
+(require 'evernote-mode)
+(global-set-key "\C-cec" 'evernote-create-note)
+(global-set-key "\C-ceo" 'evernote-open-note)
+(global-set-key "\C-ces" 'evernote-search-notes)
+(global-set-key "\C-ceS" 'evernote-do-saved-search)
+(global-set-key "\C-cew" 'evernote-write-note)
+(global-set-key "\C-cep" 'evernote-post-region)
+(global-set-key "\C-ceb" 'evernote-browser)
 
 (provide 'misc-settings)
