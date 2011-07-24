7d867dd276c6009265fd994e76fbfb1c7867d256
Link Gou <gouboft@hotmail.com>
2011-07-24 22:14:20 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -2,22 +2,30 @@
 
 echo "Install emacs config"
 echo ""
-echo "the \"emacs-config\" should place in the path: ~/.emacs.d/"
+echo "Note: the \"emacs-config\" should place in the path: ~/.emacs.d/"
 echo ""
-echo "backup the old \".emacs\" to \".emacs.backup\""
+# Step 1
+echo "1. backup the old \".emacs\" to \".emacs.backup\""
 mv ~/.emacs ~/.emacs.backup
-
 ln -s ~/.emacs.d/emacs-config/.emacs ~/.emacs
-
-echo "make dictionary for auto backup and auto save"
+echo ""
+# Step 2
+echo "2. make dictionary for auto backup and auto save"
 mkdir ~/.emacs.d/auto-backup
 mkdir ~/.emacs.d/auto-save
-
-echo "cp ecb user layouts config file to ~/"
+echo ""
+# Step 3
+echo "3. Copy ecb user layouts config file to ~/"
 cp ./tmp/.ecb-user-layouts.el ~/
+echo ""
+# Step 4
+echo "4. Install cscope and ruby"
+sudo apt-get install cscope cscope-el ruby > /dev/null
+# set for evernote
+cd ./download/evernote-0.30/ruby
+sudo ruby setup.rb
 
-echo "setup cscope"
-sudo apt-get install cscope cscope-el
+echo "alias em='emacsclient'" >> ~/.bashrc
 
 echo ""
 echo "All Done!"
