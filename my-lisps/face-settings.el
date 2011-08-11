32a080d87cb05b47de78106f647dc9dc7054e3eb
Bo Gou <bgou@hzwowpad.com>
2011-08-11 15:12:43 +0800

    modify maximized screen only in X window

--- my-lisps/face-settings.el
+++ my-lisps/face-settings.el
@@ -20,7 +20,9 @@
    		 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
     		 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
-(maximized-screen)
+(if window-system
+    (progn
+      (maximized-screen)))
 
 ;; Vim mode
 (require 'vim)
