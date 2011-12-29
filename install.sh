2682ba27de975129b61b3059030ba34890e521f4
Bo Gou <bgou@hzwowapd.com>
2011-12-29 11:19:48 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 
-echo "Install emacs config"
-echo ""
-echo "Note: the \"emacs-config\" should place in the path: ~/.emacs.d/"
-echo ""
-# Step 1
-echo "1. backup the old \".emacs\" to \".emacs.backup\""
-if [ -f ~/.emacs ]; then
-    mv ~/.emacs ~/emacs.backup
-    echo "Your ~/.emacs file is copyed to emacs.backup."
+echo "    EMACS: Install emacs config"
+
+echo "        Note: the \"emacs-config\" should place in the path: ~/.emacs.d/"
+
+# 1. link the .emacs
+if [ -e ~/.emacs ]; then
+    rm ~/.emacs
     ln -s ~/.emacs.d/emacs-config/emacs ~/.emacs
 else
     ln -s ~/.emacs.d/emacs-config/emacs ~/.emacs
 fi
 echo ""
-# Step 2
-echo "2. make dictionary for auto backup and auto save"
-if [ -d ~/.emacs.d/auto-backup ]; then
+
+# 2. make dictionary for auto backup and auto save
+if [ -e ~/.emacs.d/auto-backup ]; then
     echo "folder auto-backup is exist"
 else
     mkdir ~/.emacs.d/auto-backup
 fi
 
-if [ -d ~/.emacs.d/auto-save ]; then
+if [ -e ~/.emacs.d/auto-save ]; then
     echo "folder auto-save is exist"
 else
     mkdir ~/.emacs.d/auto-save
 fi
 echo ""
-# Step 3
-echo "3. Copy ecb user layouts config file to ~/"
+
+# 3. Copy ecb user layouts config file to ~/
 cp ./tmp/.ecb-user-layouts.el ~/
 echo ""
-# Step 4
-echo "4. Install cscope ruby w3m"
-sudo apt-get install cscope cscope-el ruby w3m w3m-el> /dev/null
+
+# 4. Install cscope ruby w3m mew
+sudo apt-get install cscope cscope-el ruby w3m w3m-el mew -y > /dev/null
 sudo cp ./tmp/cscope-indexer /usr/bin/cscope-indexer
 sudo cp ./tmp/xcscope.el  /usr/share/emacs23/site-lisp/cscope/xcscope.el
 sudo cp ./tmp/xcscope.elc /usr/share/emacs23/site-lisp/cscope/xcscope.elc
@@ -42,6 +40,5 @@ sudo cp ./tmp/xcscope.elc /usr/share/emacs23/site-lisp/cscope/xcscope.elc
 cd ./download/evernote-0.30/ruby
 sudo ruby setup.rb
 
-echo ""
-echo "All Done!"
+echo "    EMACS: Setup Done!"
 
