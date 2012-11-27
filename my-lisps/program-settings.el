3506b38c23fcddbe52997e13de02fccc1fc4d6b6
Bo Gou <link@gps-e.com>
2012-11-27 15:15:06 +0800

    modify the auto complete

--- my-lisps/program-settings.el
+++ my-lisps/program-settings.el
@@ -89,10 +89,29 @@
 (require 'llvm-mode)
 (require 'tablegen-mode)
 
+;; Auto Complete
+(require 'auto-complete-config)
+(ac-config-default)
+
 ;; Clang complete
-(load-library "clang-completion-mode")
-;(require 'clang-completion-mode)
-(define-key global-map [(meta \-)]  'clang-completion-mode)
+(require 'auto-complete-clang)
+(setq ac-auto-start nil)
+(setq ac-quick-help-delay 0.5)
+(ac-set-trigger-key "TAB")
+(define-key ac-mode-map  [(control tab)] 'auto-complete)
+(defun my-ac-config ()
+  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
+  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+  (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
+  (add-hook 'css-mode-hook 'ac-css-mode-setup)
+  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+  (global-auto-complete-mode t))
+(defun my-ac-cc-mode-setup ()
+  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
+    (add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
+;; ac-source-gtags
+(my-ac-config)
 
 ;; Template
 (require 'template)
