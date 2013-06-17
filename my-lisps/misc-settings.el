9bc0f8a15bdbe03ce874470c407b68af8b0e1387
Bo Gou <link@gps-e.com>
2013-06-17 14:01:15 +0800

    add markdown mode

--- my-lisps/misc-settings.el
+++ my-lisps/misc-settings.el
@@ -104,4 +104,13 @@
 (prefer-coding-system 'chinese-gbk)
 (prefer-coding-system 'utf-8)
 
+;; Markdown
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.mdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.Md\\'" . markdown-mode))
+
 (provide 'misc-settings)
