ebb8370e1d96757a17ecc3e2ecbe7d07a04b750e
Bo Gou <link@gps-e.com>
2013-12-12 09:56:32 +0800

    add if-endif and short key for comment

--- my-lisps/program-settings.el
+++ my-lisps/program-settings.el
@@ -123,4 +123,37 @@
 (global-set-key [?\C-c ?m] 'column-marker-1)
 (add-hook 'python-mode-hook (lambda () (interactive) (column-marker-1 80)))
 
+(defun my-c-mode-font-lock-if0 (limit)
+   (save-restriction
+     (widen)
+     (save-excursion
+       (goto-char (point-min))
+       (let ((depth 0) str start start-depth)
+         (while (re-search-forward "^\\s-*#\\s-*\\(if\\|else\\|endif\\)" limit 'move)
+           (setq str (match-string 1))
+           (if (string= str "if")
+               (progn
+                 (setq depth (1+ depth))
+                 (when (and (null start) (looking-at "\\s-+0"))
+                   (setq start (match-end 0)
+                         start-depth depth)))
+             (when (and start (= depth start-depth))
+               (c-put-font-lock-face start (match-beginning 0) 'font-lock-comment-face)
+               (setq start nil))
+             (when (string= str "endif")
+               (setq depth (1- depth)))))
+         (when (and start (> depth 0))
+           (c-put-font-lock-face start (point) 'font-lock-comment-face)))))
+   nil)
+
+(defun my-c-mode-common-hook-if0 ()
+   (font-lock-add-keywords
+    nil
+    '((my-c-mode-font-lock-if0 (0 font-lock-comment-face prepend))) 'add-to-end))
+
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook-if0)
+
+;; comment or uncomment
+(global-set-key [?\C-c ?\C-/] 'comment-or-uncomment-region)
+
 (provide 'program-settings)
